29aef61f3e2d56668fac3f9aa04ce2e9ab82759c
Waldemar Schneider <huhulasmichinruhe@web.de>
2025-01-06 22:06:09 +0100

    since sml/1.1.9 the state machine is not working correctly

--- conanfile.py
+++ conanfile.py
@@ -20,8 +20,8 @@ class Project(ConanFile):
         self.requires("catch2/2.13.7")
         self.requires("magic_enum/[>=0.9.5 <10]")
         self.requires("boost/1.86.0",force=True)
-        self.requires("confu_json/[>=1.0.1 <2]")
-        self.requires("sml/1.1.11")
+        self.requires("confu_json/[>=1.1.1 <2]")
+        self.requires("sml/1.1.8") #DO NOT CHANGE THIS. starting with version 1.1.9 process_event returns ins some cases false where before it returned true
         self.requires("durak_computer_controlled_opponent/2.2.0")
         self.requires("confu_soci/[<1]")
         self.requires("corrade/2020.06")
